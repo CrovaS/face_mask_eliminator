58d6813d4b76877276dc2baa09558b09005d0dd9
Daniel Cho <Daniel Cho>
2020-12-13 00:19:15 +0900

    forked_test

--- test.py
+++ test.py
@@ -1,5 +1,11 @@
 from Mask_RCNN import test
 import os
 
-os.chdir("Mask_RCNN")
-exec(open("test.py").read())
+pid = os.fork()
+if pid==0:
+    os.chdir("Mask_RCNN")
+    exec(open("test.py").read())
+else:
+    os.wait()
+    os.chdir("generative_inpainting")
+    exec(open("test.py").read())
